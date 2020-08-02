76bc4ba3c62c3f59473d19cd642cbac1c32845a1
LakhtionovEP <blade_lahty@mail.ru>
2020-08-02 22:39:32 +0300

    Comments

--- cian_parse.py
+++ cian_parse.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
+# получение данных с cbr.ru по ссылке
+
 
 def get_obl_cb(url):
     a = []
@@ -20,8 +22,13 @@ def get_obl_cb(url):
     stavka_cb = float(re.sub(',', '.', stavka_cb))
     return stavka_cb
 
+# получение данных с cian по ссылке
+
 
 def get_cian_data(url):
+
+    # использование регулярных выражений для очистки данных от шума
+
     filr = re.compile(r'[\"]>(.*?)</')
     filra = re.compile(r'[\"]>([0-9а-яА-Я.\W]*?)</')
     filrd = re.compile(r'>\s+(\d.*?)</')
@@ -31,7 +38,9 @@ def get_cian_data(url):
     response = requests.get(url)
     html = response.content
     soup = BeautifulSoup(html, 'html.parser')
-    #obj = dict()
+
+    # парсинг html с cian.ru
+
     obj = soup.findAll('span', attrs={'class': "a10a3f92e9--underground_time--1fKft"})
     obj2 = soup.findAll('a', attrs={'class': "a10a3f92e9--underground_link--AzxRC"})
     obj3 = soup.find('span', attrs={'itemprop': "price"})
@@ -42,55 +51,54 @@ def get_cian_data(url):
     obj8 = soup.find('h1', attrs={'class': "a10a3f92e9--title--2Widg"})
     obj9 = soup.find_all('div', attrs={'class': "a10a3f92e9--info-value--18c8R"})
     obj10 = soup.find_all('div', attrs={'class': "a10a3f92e9--offer_card_page-bti--2BrZ7"})
-    phone_str = ''
-    add_str = ''
-    metro_str = ''
+
+    phone_str, add_str, metro_str  = '', '', ''
     a = []
-    metro_time = filrd.findall(str(obj))
+    metro_time = filrd.findall(str(obj))  # время до метро
     if len(metro_time) == 0:
         metro_time = 'N/A'
     try:
-        metro_name = filr.findall(str(obj2))
+        metro_name = filr.findall(str(obj2))  # название метро
     except:
         metro_name = 'N/A'
     try:
-        flat_price = filr.findall(str(obj3))[-1]
+        flat_price = filr.findall(str(obj3))[-1]  # цена квартиры
     except:
         flat_price = 'N/A'
     try:
-        phone = filr.findall(str(obj4))
+        phone = filr.findall(str(obj4))  # телефон(-ы)
         phone_str = ',\n'.join(phone)
     except:
         phone_str = 'N/A'
     try:
-        flat_price_pm = filr.findall(str(obj5))[-1]
+        flat_price_pm = filr.findall(str(obj5))[-1]  # цена квадратного метра
     except:
         flat_price_pm = 'N/A'
     try:
-        address = filra.findall(str(obj6))
+        address = filra.findall(str(obj6))  # адресс
         if address[-1] == 'На карте':
             address.pop(-1)
         add_str = ', '.join(address)
     except:
         add_str = 'N/A'
     try:
-        square = filr.findall(str(obj7))[-1]
+        square = filr.findall(str(obj7))[-1]  # площадь
     except:
         square = 'N/A'
     try:
-        room_q = filr.findall(str(obj8))[-1].split(',')[0]
+        room_q = filr.findall(str(obj8))[-1].split(',')[0]  # кол-во комнат
     except:
         room_q = 'N/A'
     try:
-        cur_fl, ov_fl = filrfl.findall(str(obj9))[-1].split(' из ')
+        cur_fl, ov_fl = filrfl.findall(str(obj9))[-1].split(' из ')  # этаж и этажность здания
     except:
         cur_fl, ov_fl = 'N/A', 'N/A'
     try:
-        b_type = fillrtype.search(str(obj10))[0]
+        b_type = fillrtype.search(str(obj10))[0]  # тип здания
     except:
         b_type = 'N/A'
     try:
-        for i in range(len(metro_name)):
+        for i in range(len(metro_name)):  # выбор наименьшего пути до метро, путь на транспорте считается с коэф. = 4
             if 'пешком' in str(metro_time[i]):
                 a.append(int(fillmt.findall(metro_time[i])[0]))
             else:
@@ -99,6 +107,8 @@ def get_cian_data(url):
     except:
         metro_str = 'N/A'
     return metro_str, flat_price, phone_str, flat_price_pm, add_str, square, room_q, cur_fl, ov_fl, b_type
+
+# преобразование и запись данных в Excel
 
 
 def excel_output(url, column):
@@ -135,7 +145,7 @@ def excel_output(url, column):
     sheet[str(column)+'10'] = int(flat_price.replace(" ", "")[:-1])
     sheet[str(column)+'16'] = address
 
-    if metro.isdigit():
+    if metro.isdigit():  # Проверка на наличие данных (в городе может не быть метро) + разбивка по интервалам
         if metro <= 5:
             metro_time_dur = 'До 5 минут пешком'
         elif 5 < metro <= 15:
@@ -158,10 +168,12 @@ def excel_output(url, column):
     sheet[str(column)+'26'] = cur_fl
     sheet[str(column)+'27'] = ov_fl
 
+# Обработка i-го объекта
+
 
 def url_input(i):
     print(f'Вставьте ссылку на объект №{i} в ЦИАНе')
-    symb = chr(66 + int(i)*2)
+    symb = chr(66 + int(i)*2)  # преобразование для определения нужного столбца для записи в Excel
     while True:
         try:
             url = input()
@@ -175,12 +187,10 @@ def url_input(i):
             continue
 
 
-id_dollar = "R01235"
-id_evro = "R01239"
 print('Введите 1 для отображения данных на сегодня, либо дату для архива')
 daychoice = input()
 
-print('Какой формат аналогов? (4 или 5)')
+print('Какой формат аналогов? (4 или 5)')  # выбор формата и файла для записи
 while True:
     ana_qua = input()
     if ana_qua == '4':
@@ -206,7 +216,7 @@ while True:
         os.system('pause')
         continue
 
-while True:
+while True:  # пользовательский выбор по необходимости внесения объектов
     print('Данные по какому объекту нужно внести? (0 - чтобы внести все)')
     obj_choice = input()
     if int(obj_choice) in range(int(ana_qua)+1):
@@ -227,26 +237,24 @@ while True:
         print('Вы ввели некорректное значение')
         continue
 
+# запись выбранной пользователем даты
 if daychoice == '1':
     todayd = datetime.date.today().strftime('%d/%m/%Y')
 else:
     todayd = daychoice
 sheet['C1'] = todayd
-#print('Сегодня ' + str(todayd))
 
+# получение данных по курсу доллару на требуемый день
 valuta = ET.parse(urllib.request.urlopen("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + str(todayd)))
-#print('Данные на ' + re.sub('\.','/', valuta.getroot().attrib['Date']))
+id_dollar = "R01235"
 for line in valuta.findall('Valute'):
     id_v = line.get('ID')
     if id_v == id_dollar:
         rub = float(re.sub(',','.',line.find('Value').text))
         sheet['C2'] = rub
-        #print("\nКурс доллара", format(rub, '.2f'), "рублей")
-#    if id_v == id_evro:
-#        rub = float(re.sub(',','.',line.find('Value').text))
-#        print("\nКурс евро", format(rub, '.2f'), "рублей")
 
 
+# получение безрисковой ставки на требуемый день (данные часто запаздывают, поэтому используется бесконечный спуск)
 while True:
     try:
         stavka_cb = get_obl_cb('https://cbr.ru/hd_base/zcyc_params/zcyc/?DateTo=' + str(todayd))
